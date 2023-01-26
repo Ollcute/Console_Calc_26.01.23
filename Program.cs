002ee1f2ef8628970b03c559b99a46d33ffa6a56
Olesia Shevtsova <ollcute.shevtsova@yandex.ru>
2023-01-26 13:38:17 +0300

    New

--- Program.cs
+++ Program.cs
@@ -11,9 +11,20 @@ namespace ConsoleApp2
         static void Main(string[] args)
 
         {
-            double a = 20; double b = 10; double c = 0;
+            double a = 7; double b = 6; double c = 0; double d = 0; double f = 0; double g = 0; double j = 0; double k = 0;
             c = a + b;
+            d = a - b;
+            f = a * b;
+            g = a / b;
+            j = a % b;
+            k = Math.Sin(a);
+            //after reset
             Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(f);
+            Console.WriteLine(g);
+            Console.WriteLine(j);
+            Console.WriteLine(k);
             Console.ReadLine();
 
         }
